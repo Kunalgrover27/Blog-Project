32dbc317fc2b3d8805199679deb2806349704052
Kunalgrover27 <kunalgrover.2704@gmail.com>
2024-03-13 15:53:13 +0530

    first Commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
+import AuthLayout from "./AuthLayout";
+import PostCard from "./PostCard";
+import PostForm from "./PostForm/PostForm";
+import Signup from "./Signup";
+import Login from "./Login"
+
+
+
+export {
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE,
+    AuthLayout,
+    PostCard,
+    PostForm,
+    Signup,
+    Login,
+    Header
+}
